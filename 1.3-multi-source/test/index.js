bb37f706be3492bcc4d6dd93297111ff70135d84
Yuanchieh <sj82516@gmail.com>
2021-10-09 19:18:36 +0800

    feat: finish 1.3

---
+++ 1.3-multi-source/test/index.js
@@ -0,0 +1,41 @@
+const mysql = require('mysql2/promise');
+const {
+    connectSourceDB,
+    connectReplicaDB,
+    setUpReplication,
+    closeConnection
+} = require('../../common');
+
+async function main() {
+    await loop(3307, "test", "source");
+    await loop(3309, "test2", "source2"); 
+}
+
+main();
+
+async function loop(sourcePort, name, source) {
+    const sourceDBConnection = await connectSourceDB('localhost', sourcePort);
+    const replicaDBConnection = await connectReplicaDB('localhost');
+    await setUpReplication(sourceDBConnection, replicaDBConnection, source, source);
+    await createDBAndInsertData(sourceDBConnection, name);
+    await new Promise(resolve => setTimeout(resolve, 5000)); 
+    await writeToSource(sourceDBConnection, name);
+    await checkDataInReplica(replicaDBConnection, name);
+    await closeConnection(sourceDBConnection, replicaDBConnection);
+}
+
+async function createDBAndInsertData(sourceDBConnection, name) {
+    await sourceDBConnection.execute(`CREATE SCHEMA IF NOT EXISTS ${name}`);
+    await sourceDBConnection.execute(`CREATE TABLE IF NOT EXISTS ${name}.${name} ( id int AUTO_INCREMENT, name varchar(255), PRIMARY KEY (id) )`);
+    await sourceDBConnection.execute(`INSERT INTO ${name}.${name} (name) VALUES ('hello') `);
+}
+
+async function writeToSource(sourceDBConnection, name) {
+    await sourceDBConnection.execute(`INSERT INTO ${name}.${name} (name) VALUES ('hello3') `);
+    await sourceDBConnection.execute(`INSERT INTO ${name}.${name} (name) VALUES ('hello4') `);
+}
+
+async function checkDataInReplica(replicaDBConnection, name) {
+    let [rows] = await replicaDBConnection.execute(`SELECT * FROM ${name}.${name}`);
+    console.log(rows);
+}
